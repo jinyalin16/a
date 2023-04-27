75f99abcfb7bd9d17ef74c76efa843f27f80bf10
jinyalin16 <131965612+jinyalin16@users.noreply.github.com>
2023-04-27 19:20:35 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
 })
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+})
 basic.forever(function () {
 	
 })
